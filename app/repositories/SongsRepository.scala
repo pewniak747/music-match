cfacbe9e81f86e81f1693ca0a5dcd1a4dd39411b
Tomasz Pewiński <pewniak747@gmail.com>
2013-12-30 19:24:45 +0100

    finding songs by title

---
+++ app/repositories/SongsRepository.scala
@@ -0,0 +1,21 @@
+package org.musicmatch.repositories
+
+import org.musicmatch.models.Song
+import org.musicmatch.models.Artist
+
+import anorm._
+import play.api.db.DB
+import play.api.Play.current
+
+object SongsRepository {
+  lazy val findByTitleQuery = SQL("SELECT * FROM songs INNER JOIN artists ON artists.id = songs.artist_id WHERE title LIKE {title}")
+
+  def findByTitle(title: String) = DB.withConnection { implicit c =>
+    findByTitleQuery.on("title" -> toParameterValue("%" + title + "%"))().map {
+      row => {
+        val artist = Artist(row[Long]("artists.id"), row[String]("artists.name"), Some(row[String]("artists.image_url")))
+        Song(row[Long]("songs.id"), row[String]("songs.title"), artist)
+      }
+    }
+  }
+}
