2711ed16bfd06964637a1658c8769efda8f60cb4
Tomasz Pewiński <pewniak747@gmail.com>
2013-12-31 17:36:20 +0100

    case insensitive search

--- app/repositories/SongsRepository.scala
+++ app/repositories/SongsRepository.scala
@@ -8,7 +8,7 @@ import play.api.db.DB
 import play.api.Play.current
 
 object SongsRepository {
-  lazy val findByTitleQuery = SQL("SELECT * FROM songs INNER JOIN artists ON artists.id = songs.artist_id WHERE title LIKE {title}")
+  lazy val findByTitleQuery = SQL("SELECT * FROM songs INNER JOIN artists ON artists.id = songs.artist_id WHERE title ILIKE {title}")
 
   def findByTitle(title: String) = DB.withConnection { implicit c =>
     findByTitleQuery.on("title" -> toParameterValue("%" + title + "%"))().map {
