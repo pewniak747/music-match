35f199fd403595a81b795fe36ce9ed6bdd2d66b6
Tomasz Pewiński <pewniak747@gmail.com>
2014-01-01 18:31:38 +0100

    allow all origins on dev

---
+++ app/controllers/filters/CorsFilter.scala
@@ -0,0 +1,17 @@
+import play.api._
+import play.api.mvc._
+import play.api.Play.current
+import play.api.http.HeaderNames._
+import play.api.libs.concurrent.Execution.Implicits._
+
+object Global extends GlobalSettings {
+  override def doFilter(action: EssentialAction) = EssentialAction { request =>
+    if (Play.isDev) {
+      action(request).map(_.withHeaders(
+        ACCESS_CONTROL_ALLOW_ORIGIN -> "*"
+      ))
+    } else {
+      action(request)
+    }
+  }
+}
