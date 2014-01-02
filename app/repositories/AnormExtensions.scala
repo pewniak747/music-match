fe7c3fe89899fa6eb261b47c0c6b7ac50270b138
Tomasz Pewiński <pewniak747@gmail.com>
2014-01-02 22:34:08 +0100

    creating scrobbles - for real

---
+++ app/repositories/AnormExtensions.scala
@@ -0,0 +1,25 @@
+package org.musicmatch.repositories
+
+import org.joda.time._
+import org.joda.time.format._
+import anorm._
+
+object AnormExtensions {
+  val dateFormatGeneration: DateTimeFormatter = DateTimeFormat.forPattern("yyyyMMddHHmmssSS")
+
+  implicit def rowToDateTime: Column[DateTime] = Column.nonNull { (value, meta) =>
+    val MetaDataItem(qualified, nullable, clazz) = meta
+    value match {
+      case ts: java.sql.Timestamp => Right(new DateTime(ts.getTime))
+      case d: java.sql.Date => Right(new DateTime(d.getTime))
+      case str: java.lang.String => Right(dateFormatGeneration.parseDateTime(str))
+      case _ => Left(TypeDoesNotMatch("Cannot convert " + value + ":" + value.asInstanceOf[AnyRef].getClass) )
+    }
+  }
+
+  implicit val dateTimeToStatement = new ToStatement[DateTime] {
+    def set(s: java.sql.PreparedStatement, index: Int, aValue: DateTime): Unit = {
+      s.setTimestamp(index, new java.sql.Timestamp(aValue.withMillisOfSecond(0).getMillis()) )
+    }
+  }
+}
--- app/repositories/ScrobblesRepository.scala
+++ app/repositories/ScrobblesRepository.scala
@@ -4,15 +4,26 @@ import org.musicmatch.models.Scrobble
 import org.musicmatch.models.Song
 import org.musicmatch.models.Artist
 
+import org.musicmatch.repositories.AnormExtensions._
+
 import anorm._
 import play.api.db.DB
 import play.api.Play.current
 import com.github.nscala_time.time.Imports._
 
 object ScrobblesRepository {
+  lazy val createQuery = SQL("INSERT INTO scrobbles (user_id, song_id, created_at) VALUES({userId}, {songId}, {createdAt})")
+  lazy val findByIdQuery = SQL("SELECT * FROM scrobbles INNER JOIN songs ON scrobbles.song_id = songs.id INNER JOIN artists ON songs.artist_id = artists.id WHERE scrobbles.id = {id}")
+
   def create(userId: Long, songId: Long) = DB.withConnection { implicit c =>
-    val artist = Artist(1, "artist name", Some("http://"))
-    val song = Song(songId, "song title", artist)
-    Some(Scrobble(1, DateTime.now, song, userId))
+    createQuery.on("userId" -> userId, "songId" -> songId, "createdAt" -> toParameterValue(DateTime.now)).executeInsert().map { id =>
+      findByIdQuery.on("id" -> id)().map { row =>
+        val artist = Artist(row[Long]("artists.id"), row[String]("artists.name"), Some(row[String]("artists.image_url")))
+        val song = Song(row[Long]("songs.id"), row[String]("songs.title"), artist)
+        Scrobble(row[Long]("scrobbles.id"), row[DateTime]("scrobbles.created_at"), song, row[Long]("scrobbles.user_id"))
+      }.headOption
+    }.getOrElse {
+      None
+    }
   }
 }
