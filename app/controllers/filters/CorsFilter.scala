d6d05e94b74ed57389f0be80579964e17428e161
Tomasz Pewiński <pewniak747@gmail.com>
2014-01-04 22:05:38 +0100

    cors

--- app/controllers/filters/CorsFilter.scala
+++ app/controllers/filters/CorsFilter.scala
@@ -6,13 +6,9 @@ import play.api.libs.concurrent.Execution.Implicits._
 
 object Global extends GlobalSettings {
   override def doFilter(action: EssentialAction) = EssentialAction { request =>
-    if (Play.isDev) {
-      action(request).map(_.withHeaders(
-        ACCESS_CONTROL_ALLOW_ORIGIN -> "*",
-        ACCESS_CONTROL_ALLOW_HEADERS -> "Content-Type"
-      ))
-    } else {
-      action(request)
-    }
+    action(request).map(_.withHeaders(
+      ACCESS_CONTROL_ALLOW_ORIGIN -> "*",
+      ACCESS_CONTROL_ALLOW_HEADERS -> "Content-Type"
+    ))
   }
 }
