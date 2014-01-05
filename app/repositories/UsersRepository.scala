fbaca2c82a256b9fa5c81855ee3d72f3844c5108
Tomasz Pewiński <pewniak747@gmail.com>
2014-01-05 01:55:48 +0100

    finding user by id

--- app/repositories/UsersRepository.scala
+++ app/repositories/UsersRepository.scala
@@ -9,9 +9,16 @@ import org.musicmatch.repositories.AnormExtensions._
 
 object UsersRepository {
   lazy val findByEmailQuery = SQL("SELECT * FROM users WHERE users.email = {email} LIMIT 1")
+  lazy val findByIdQuery = SQL("SELECT * FROM users WHERE users.id = {id} LIMIT 1")
 
   def findByEmail(email: String) = DB.withConnection { implicit c =>
     findByEmailQuery.on("email" -> email)().map { row =>
+      new UserMapper(row).get
+    }.headOption
+  }
+
+  def findById(id: Long) = DB.withConnection { implicit c =>
+    findByIdQuery.on("id" -> id)().map { row =>
       new UserMapper(row).get
     }.headOption
   }
