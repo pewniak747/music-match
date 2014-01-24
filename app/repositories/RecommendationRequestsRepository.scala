bef2f2d12f462eab8d9bf6994b91e47148785e03
Tomasz Pewiński <pewniak747@gmail.com>
2014-01-24 23:07:17 +0100

    mark requests as enqueued

--- app/repositories/RecommendationRequestsRepository.scala
+++ app/repositories/RecommendationRequestsRepository.scala
@@ -8,15 +8,19 @@ import play.api.Play.current
 
 object RecommendationRequestsRepository {
   lazy val createQuery = SQL("INSERT INTO recommendation_requests(user_id) VALUES({userId})")
-  lazy val newestQuery = SQL("SELECT * FROM recommendation_requests ORDER BY created_at ASC LIMIT {limit}")
+  lazy val newestQuery = SQL("SELECT * FROM recommendation_requests WHERE enqueued_at IS NULL ORDER BY created_at ASC LIMIT {limit} FOR UPDATE")
+  lazy val enqueueQuery = (ids: List[Long]) => SQL("UPDATE recommendation_requests SET enqueued_at = current_timestamp WHERE id IN (%s)".format(ids.mkString(",")))
 
   def create(userId: Long) = DB.withConnection { implicit c =>
     createQuery.on("userId" -> userId).executeInsert()
   }
 
-  def newest(limit: Int = 5) = DB.withConnection { implicit c =>
-    newestQuery.on("limit" -> limit)().map { row =>
+  def newest(limit: Int = 5) = DB.withTransaction { implicit c =>
+    val batch = newestQuery.on("limit" -> limit)().map { row =>
       new RecommendationRequestMapper(row).get
     }.toList
+    if (!batch.isEmpty)
+      enqueueQuery(batch.map(_.id)).executeUpdate()
+    batch
   }
 }
