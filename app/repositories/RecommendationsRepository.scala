8080c3f29862447a6cecdcba71a940b3f42d2744
Tomasz Pewiński <pewniak747@gmail.com>
2014-01-24 23:59:37 +0100

    insert recommendations

--- app/repositories/RecommendationsRepository.scala
+++ app/repositories/RecommendationsRepository.scala
@@ -8,10 +8,17 @@ import org.musicmatch.repositories.AnormExtensions._
 
 object RecommendationsRepository {
   lazy val findByUserIdQuery = SQL("SELECT * FROM recommendations INNER JOIN songs ON recommendations.song_id = songs.id INNER JOIN artists ON songs.artist_id = artists.id WHERE recommendations.user_id = {userId} ORDER BY created_at DESC")
+  lazy val insertQuery = SQL("INSERT INTO recommendations(user_id, song_id, created_at) VALUES({userId}, {songId}, current_timestamp)")
 
   def findByUserId(userId: Long) = DB.withConnection { implicit c =>
     findByUserIdQuery.on("userId" -> userId)().map { row =>
       new RecommendationMapper(row).get
     }.toList
+  }
+
+  def insert(userId: Long, songIds: Seq[Long]) = DB.withTransaction { implicit c =>
+    songIds.foreach { id =>
+      insertQuery.on("userId" -> userId, "songId" -> id).executeInsert()
+    }
   }
 }
